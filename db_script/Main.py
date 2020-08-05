42e5f1683ed02d91d1e7fc3dba69d92171efb1c0
Nguyen Viet Long <nvlong17@gmail.com>
2020-08-05 16:16:07 +0700

    Changes to db_script

--- db_script/Main.py
+++ db_script/Main.py
@@ -12,13 +12,13 @@ OTX_TAXI_CLIENT = {"hostURL":'otx.alienvault.com'
                    , "collectionNames": ['user_maldatabase']
                    }
 
-ESSET_TAXI_CLIENT = {"hostURL":'eti.eset.com'
-                   , "useHHTPS":True
-                   , "discoveryPath":'/taxiiservice/discovery'
-                   , "usernameKey" : "hieutt35@fpt.com.vn"
-                   , "password" : 'X^7PGvy0$c3$Kzk'
-                   , "collectionNames": ['ei.urls','ei.domains v2']
-                   }
+#ESSET_TAXI_CLIENT = {"hostURL":'eti.eset.com'
+#                   , "useHHTPS":True
+#                   , "discoveryPath":'/taxiiservice/discovery'
+#                   , "usernameKey" : "hieutt35@fpt.com.vn"
+#                   , "password" : 'X^7PGvy0$c3$Kzk'
+#                   , "collectionNames": ['ei.urls','ei.domains v2']
+#                   }
 
 #------------- DATE -------------
 #Must be in format: YYYY-MM-DDTHH:MM:SS.ssssss+/-hh:mm (ISO8601)
@@ -47,15 +47,15 @@ alientvaultTaxiAndDatabaseConnect = TaxiClientToDatabase(OTX_TAXI_CLIENT['hostUR
                                                          , OTX_TAXI_CLIENT['password']
                                                          , OTX_TAXI_CLIENT['collectionNames'],databaseClient,timeManager)
 
-esetTaxiAndDatabaseConnect = TaxiClientToDatabase(ESSET_TAXI_CLIENT['hostURL']
-                                                         , ESSET_TAXI_CLIENT['useHHTPS']
-                                                         , ESSET_TAXI_CLIENT['discoveryPath']
-                                                         , ESSET_TAXI_CLIENT['usernameKey']
-                                                         , ESSET_TAXI_CLIENT['password']
-                                                         , ESSET_TAXI_CLIENT['collectionNames'],databaseClient,timeManager)
+#esetTaxiAndDatabaseConnect = TaxiClientToDatabase(ESSET_TAXI_CLIENT['hostURL']
+#                                                         , ESSET_TAXI_CLIENT['useHHTPS']
+#                                                         , ESSET_TAXI_CLIENT['discoveryPath']
+#                                                         , ESSET_TAXI_CLIENT['usernameKey']
+#                                                         , ESSET_TAXI_CLIENT['password']
+#                                                         , ESSET_TAXI_CLIENT['collectionNames'],databaseClient,timeManager)
 
 #Download STIXX File and Insert to database
-esetTaxiAndDatabaseConnect.get_STIX_and_insert_database()
+#esetTaxiAndDatabaseConnect.get_STIX_and_insert_database()
 alientvaultTaxiAndDatabaseConnect.get_STIX_and_insert_database()
 #Update time
 timeManager.write_time_to_file()
