6337d7e55a3a0f409805416cb791a62f108319f4
minhvv13 <minhvv13@fpt.com.vn>
2020-11-17 00:02:06 +0700

    add new feed twitter ioc

--- db_script/Main.py
+++ db_script/Main.py
@@ -1,45 +1,61 @@
+from TwitterIOC import TwitterIOC
 from TaxiExecute import TaxiClientToDatabase
 from DatabaseClient import DatabaseConnect
 from TimeManager import TimeManager
 
-############# CONFIG ############# 
-#------------- TAXII CLIENT -------------
-OTX_TAXI_CLIENT = {"hostURL":'otx.alienvault.com'
-                   , "useHHTPS":True
-                   , "discoveryPath":'taxii/discovery'
-                   , "usernameKey" : "e3a88efb05eb78a2c2affd8e78598fef969643976b06a9ad3998051fa2f37a15"
-                   , "password" : 'X^7PGvy0$c3$Kzk'
-                   , "collectionNames": ['user_maldatabase']
-                   }
-                   
-#------------- DATE -------------
-#Must be in format: YYYY-MM-DDTHH:MM:SS.ssssss+/-hh:mm (ISO8601)
-#To specify specific time, use BEGIN_DATE. Otherwise, script will find a file 
+############# CONFIG #############
+# ------------- TAXII CLIENT -------------
+OTX_TAXI_CLIENT = {
+    "hostURL": 'otx.alienvault.com',
+    "useHHTPS": True,
+    "discoveryPath": 'taxii/discovery',
+    "usernameKey": "e3a88efb05eb78a2c2affd8e78598fef969643976b06a9ad3998051fa2f37a15",
+    "password": 'X^7PGvy0$c3$Kzk',
+    "collectionNames": ['user_maldatabase'],
+}
+
+TWITTER_IOC = {"hostURL": "http://tweettioc.com/feed/domain"}
+# ------------- DATE -------------
+# Must be in format: YYYY-MM-DDTHH:MM:SS.ssssss+/-hh:mm (ISO8601)
+# To specify specific time, use BEGIN_DATE. Otherwise, script will find a file
 # -- If LAST_TIME_UPDATE.txt file NOT FOUND and BEGIN_DATE = None, script will Download All
 BEGIN_DATE = None
 
-#------------- DATABASE -------------
+# ------------- DATABASE -------------
 DATABASE_HOST_NAME = "localhost"
-DATABASE_USERNAME="root"
-DB_USER_PASSWORD="password"
+DATABASE_USERNAME = "root"
+DB_USER_PASSWORD = "password"
 DATABASE_NAME = "DomainServer"
 AUTH_PLUGIN = "mysql_native_password"
 
 
 #################### MAIN ########################
-#Create a time manager
+# Create a time manager
 timeManager = TimeManager(BEGIN_DATE)
-#Create a database Client
-databaseClient = DatabaseConnect(DATABASE_HOST_NAME, DATABASE_USERNAME, DB_USER_PASSWORD, DATABASE_NAME, AUTH_PLUGIN)
-#Create a AlientVault TAXII Client and Connect to database
-alientvaultTaxiAndDatabaseConnect = TaxiClientToDatabase(OTX_TAXI_CLIENT['hostURL']
-                                                         , OTX_TAXI_CLIENT['useHHTPS']
-                                                         , OTX_TAXI_CLIENT['discoveryPath']
-                                                         , OTX_TAXI_CLIENT['usernameKey']
-                                                         , OTX_TAXI_CLIENT['password']
-                                                         , OTX_TAXI_CLIENT['collectionNames'],databaseClient,timeManager)
+# Create a database Client
+databaseClient = DatabaseConnect(
+    DATABASE_HOST_NAME, DATABASE_USERNAME, DB_USER_PASSWORD, DATABASE_NAME, AUTH_PLUGIN
+)
+# Create a AlientVault TAXII Client and Connect to database
+alientvaultTaxiAndDatabaseConnect = TaxiClientToDatabase(
+    OTX_TAXI_CLIENT['hostURL'],
+    OTX_TAXI_CLIENT['useHHTPS'],
+    OTX_TAXI_CLIENT['discoveryPath'],
+    OTX_TAXI_CLIENT['usernameKey'],
+    OTX_TAXI_CLIENT['password'],
+    OTX_TAXI_CLIENT['collectionNames'],
+    databaseClient,
+    timeManager,
+)
 
-#Download STIXX File and Insert to database
+# Download STIXX File and Insert to database
 alientvaultTaxiAndDatabaseConnect.get_STIX_and_insert_database()
-#Update time
+
+# Create a Twitter IOC client and connect to database
+twitterIOCAndDatabaseConnect = TwitterIOC(hostURL=TWITTER_IOC["hostURL"], myDatabase=databaseClient)
+
+# Downloaf a text file and insert to database
+twitterIOCAndDatabaseConnect.get_domain_and_insert_database()
+
+# Update time
 timeManager.write_time_to_file()
---
+++ db_script/TwitterIOC.py
@@ -0,0 +1,44 @@
+from DatabaseClient import DatabaseConnect
+from Ultility import Ultility
+from TimeManager import TimeManager
+import warnings
+
+
+class TwitterIOC:
+    def __init__(
+        self,
+        hostURL,
+        myDatabase,
+    ):
+        self.hostURL = hostURL
+        self.myDatabase = myDatabase
+
+    # Poll content for AlientVault Taxi Server
+    def get_domain_and_insert_database(self):
+        Ultility.print_and_write_to_log(
+            "-------------------- HOST URL: "
+            + self.hostURL
+            + "----------------------\n"
+        )
+        # Ingnore Warning
+        warnings.filterwarnings("ignore")
+        Ultility.print_and_write_to_log(
+            "-------------------- Collection Name: "
+            + "Twitter IOC"
+            + "----------------------\n"
+        )
+        blockOfXML = 0
+        try:
+            blockOfXML += 1
+            domains = []
+            # Convert XML data to list of domains
+            domains = Ultility.get_domain_from_text(self.hostURL)
+            # Loop through each domain in the list
+            (
+                numberOfDomains,
+                duplicatedDomains,
+            ) = self.myDatabase.load_to_database(domains)
+            # Print result to Terminal
+            Ultility.print_result(blockOfXML, numberOfDomains, duplicatedDomains)
+        except Exception as e:
+            Ultility.print_error(blockOfXML, str(e))
